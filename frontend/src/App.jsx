47fb19eb5c6b28f8aa3927e5304c460cba274ea6
Joao Carlos Dene <joaoo.c@hotmail.com>
2022-01-29 17:21:25 -0300

    renderizaçao de componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
 
 import './App.css';
+import React from 'react';
 
-function App() {
+import Logo from './components/logo/index'
+import Nav from './components/nav/index'
+import Main from './components/main/index'
+import Footer from './components/footer/index'
+
+
+export default () =>{
   return (
-    <div >
-     Hello World
-    </div>
-  );
+    <div className="app">
+      <Logo/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+  </div>
+  )
 }
+  
 
-export default App;
---
+++ frontend/src/components/main/index.jsx
@@ -0,0 +1,12 @@
+import './main.css'
+import React from 'react'
+import Header from '../header/index'
+
+
+export default props =>
+    <React.Fragment>
+        <Header/>
+        <main className="content">
+            Conteudo
+        </main>
+    </React.Fragment>
